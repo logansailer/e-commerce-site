3bd00f8ca1b2955867d509459bb277c8a956d7bd
logansailer <logansailer0633@gmail.com>
2025-11-03 17:19:37 -0500

    deleted unnecessary files

--- e-commerce-site/src/assets/index.js
+++ e-commerce-site/src/assets/index.js
@@ -2,8 +2,8 @@ import cartDark from "./cartDark.png";
 import cartLight from "./cartLight.png";
 import googleDark from "./googleDark.png";
 import googleLight from "./googleLight.png";
-import logoDark from "./logoDark.png";
-import logoLight from "./logoLight.png";
+import logoDark from "./logo.png";
+import logoLight from "./logo.png";
 import paymentIcons from "./paymentIcons.png";
 
 export {
